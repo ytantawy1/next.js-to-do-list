48103cf4e34b6ca90fef0f22dff11184f74fa437
ytantawy1 <113364714+ytantawy1@users.noreply.github.com>
2024-04-23 19:04:41 -0600

    delete functionality added

--- components/forms/EditTaskForm.jsx
+++ components/forms/EditTaskForm.jsx
@@ -1,61 +1,38 @@
 'use client'
 import {useFormState} from 'react-dom'
-import {useState} from 'react'
-
+import { useState } from 'react'
 import {toast} from 'sonner'
-
-import {editAction} from '@/actions/editTaskAction'
+import { editAction } from '@/actions/editTaskAction'
 import {cn} from '@/lib/utils/mergeCss'
+
 import {FormControl, Input, Label} from '.'
 
 const initialState = {
 	message: 'default',
 }
 
-/* 
-             Communication between client - server 
-
-			 use the useFormState()  hook.
-			 const anyInitialState = {
-				property:value,
-				prop:value
-			 }
-
-			 const [state, formActionToRun] = useFormState(yourServerActionToRun, anyInitialState)
-			
-			
-			 <form action={formActionToRun}>
-
-
-			 Client sending to the Server
-			  <form action={yourServerAction}>
-
-
- 
- 
-*/
 function EditTaskForm({children, className, uid, payload}) {
 	// formAction is for server and client communication
 	const [state, formAction] = useFormState(editAction, initialState)
-    const [category, setCategory] = useState(payload.category)
-    const [task, setTask] = useState(payload.task)
+	const [category, setCategory] = useState(payload.category)
+	const [task, setTask] = useState(payload.task)
 
-    function handleInput(e) {
-        switch (e.currentTarget.name) {
-           case 'category':
-          setCategory(e.currentTarget.value)
-          break
-        case 'task':
-            setTask(e.currentTarget.value)
-            break
-        default:
-            null
-    }
-}
+	function handleInput(e) {
+		switch (e.currentTarget.name) {
+			case 'category':
+				setCategory(e.currentTarget.value)
+				break
+			case 'task':
+				setTask(e.currentTarget.value)
+				break
+			default:
+				null
+		}
+	}
 
 	if (state.message === 'success') {
 		toast(
-			<aside className="bg-green-500 text-lime-50 rounded-lg py-6 text-center">
+			<aside className="bg-emerald-200 text-zinc-900 rounded-lg py-6 text-center">
 				<p> your toast message was saved successfully</p>
 			</aside>
 		)
@@ -69,21 +46,20 @@ function EditTaskForm({children, className, uid, payload}) {
 				</h2>
 			</header>
 			<form action={formAction} className={cn('space-y-5  bg-white    py-8 px-4', className)}>
-			
-            <FormControl >
-					
-					<Input type="hidden" name="uid" value={uid}/>
+
+				<FormControl>
+					<Input type="hidden" name="uid" value={uid} />
 				</FormControl>
-            
-            	<FormControl className="flex flex-col">
+
+				<FormControl className="flex flex-col">
 					<Label htmlFor="category">Category</Label>
-					<Input onInput={handleInput} id="category" name="category" placeholder="enter the task category" />
+					<Input  onInput={handleInput} id="category" name="category" defaultValue={category} />
 				</FormControl>
 
 				<FormControl className="flex flex-col">
 					<Label htmlFor="task">Task</Label>
-					<Input onInput={handleInput} id="task" name="task" placeholder="enter a new task" />
-				</FormControl> 
+					<Input onInput={handleInput} id="task" name="task" defaultValue={task} />
+				</FormControl>
 				<FormControl className="pt-3">
 					<button className="bg-black text-white w-full py-2.5 rounded-lg mt-3 font-semibold">
 						Update Task
